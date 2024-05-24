5a5e98437d23769889f894738d2bb86314a699a6
adzibilal <adzibilal02@gmail.com>
2024-05-24 16:41:14 +0700

    change vercel

--- api/index.php
+++ api/index.php
@@ -1,11 +1,17 @@
 <?php
 
-require '../vendor/autoload.php';
-$app = require_once '../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../vendor/autoload.php';
 
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+
+$response = $kernel->handle(
+    $request
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
